728c2d5f6309b96af2d086bc881b666aa6774756
GzSpace <fanguizhang@163.com>
2015-03-18 10:32:44 +0800

    首次修改

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -1,15 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿// <copyright file="Program.cs" company="Fangz@China">
+//  Copyright (c) Fangz@China. All rights reserved.
+// </copyright>
+// <author>fangz</author>
+// <date> 2015/03/18 10:28:41 </date>
+// <summary> 测试程序 </summary>
+// <modify>
+//      修改人：fangz
+//      修改时间：2015/03/18 10:28:41
+//      修改描述：新建 Program.cs
+//      版本：1.0
+// </modify>
 namespace ConsoleTest
 {
-    class Program
+    /// <summary>
+    /// The program.
+    /// </summary>
+    internal class Program
     {
-        static void Main(string[] args)
+        #region Methods
+
+        /// <summary>
+        /// The main.
+        /// </summary>
+        /// <param name="args">
+        /// The args.
+        /// </param>
+        private static void Main(string[] args)
         {
         }
+
+        #endregion
     }
 }
